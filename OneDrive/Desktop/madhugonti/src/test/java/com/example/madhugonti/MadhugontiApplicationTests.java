3eb3f1744b76af0c085fd3ce2f5b393d3f4de5e5
anjaneyulu77 <112678403+anjaneyulu77@users.noreply.github.com>
2022-12-11 09:38:06 +0530

    fixed package issue

---
+++ OneDrive/Desktop/madhugonti/src/test/java/com/example/madhugonti/MadhugontiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.madhugonti;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MadhugontiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
