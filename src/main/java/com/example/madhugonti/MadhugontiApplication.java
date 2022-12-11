7945a6e73ff681494e9e511b84887e51484a992e
anjaneyulu77 <112678403+anjaneyulu77@users.noreply.github.com>
2022-12-11 10:02:29 +0530

    created new Second main class

--- src/main/java/com/example/madhugonti/MadhugontiApplication.java
+++ src/main/java/com/example/madhugonti/MadhugontiApplication.java
@@ -1,14 +1,14 @@
-package com.example.madhugonti;
-
-import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
-
-@SpringBootApplication
-public class MadhugontiApplication {
-
-	public static void main(String[] args) {
-		SpringApplication.run(MadhugontiApplication.class, args);
-		System.out.println("welcome to Hyd:::::::::");
-	}
-
-}
+/*
+ * package com.example.madhugonti;
+ * 
+ * import org.springframework.boot.SpringApplication; import
+ * org.springframework.boot.autoconfigure.SpringBootApplication;
+ * 
+ * @SpringBootApplication public class MadhugontiApplication {
+ * 
+ * public static void main(String[] args) {
+ * SpringApplication.run(MadhugontiApplication.class, args);
+ * System.out.println("welcome to Hyd:::::::::"); }
+ * 
+ * }
+ */
